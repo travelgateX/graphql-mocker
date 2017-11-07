aba0fa37425c528e46aee664c10c9b9271199432
ngonzalez <ngonzalez@xmltravelgate.com>
2017-11-07 11:23:58 +0100

    Added information on Mock's help

--- tools/graph_mocker/help.js
+++ tools/graph_mocker/help.js
@@ -3,6 +3,6 @@ module.exports.printMockerHelp = function() {
     console.log("       Mocks a schema and/or extends it with another one.\n");
     console.log("           <directory>: Path to the directory containing the schema/s.");
     console.log("           <apiName>: Name of the API to extend.");
-    console.log("           <workingAPIs>: List of APIs that will be merged as base APIs (e.g. [api1, api2,..., apin])");
+    console.log("           <workingAPIs>: List of APIs (separated with ',') that will be merged as base APIs (e.g. api1,api2,...,apin)");
     console.log("\n\n")
 }
