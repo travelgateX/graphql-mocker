45d982c671867187bf0a2f2c8aa334ac29de10cb
Amian <damian@xmltravelgate.com>
2018-09-19 16:54:39 +0200

    Refactor code of merger, now the merge do with AST objects and allow object types without fields to expand later

--- tools/schema_merger/merger.js
+++ tools/schema_merger/merger.js
@@ -1,16 +1,34 @@
 module.exports = {
-    main: main
+    main: main,
+    mergeAST: mergeAST
 };
 
+const convertLegacyDescriptions = require('../schema_splitter/splitter').convertLegacyDescriptions;
 const fs = require('fs');
 const { printMergeHelp } = require('./help')
-
+const graphqllang= require('graphql/language');
+const graphql= require('graphql');
 //Saved split types
 const types = ["commons", "interfaces", "objects", "inputs", "scalars", "enums"];
 //Extendible types
 var sourceFile = require('../../sourceFile');
 var extendibles = sourceFile.extendibles;
 var extendedTypes = {};
+
+function mergeAST(dirs,outputPath, overWrite){
+    var completeAST=null;
+    dirs.forEach(function (dir) {
+        //Get directory name for comparison
+        
+        console.log("Proceeding to merge schema at " + dir)
+        var partialAST = main(dir, outputPath, overWrite);
+        if (partialAST){
+            completeAST = completeAST!==null?graphql.concatAST([completeAST, partialAST]):partialAST;
+        }
+        
+    });
+    return completeAST;
+}
 
 function main(splitPath, outputPath, overWrite, avoidExtendibles) {
     //If --h/--help, show help and exit
@@ -24,84 +42,58 @@ function main(splitPath, outputPath, overWrite, avoidExtendibles) {
         return;
     }
 
-    extendedTypes = {}; //Dictionary where extended types will be stored
-
     //Path check and instantiation
     var path;
-    splitPath = (splitPath.substr(splitPath.length - 1) === "/") ? splitPath : splitPath + "/"
-    if (outputPath) path = (outputPath.substr(outputPath.length - 1) === "/") ? outputPath : outputPath + "/";
-    else path = splitPath;
+    splitPath = splitPath.endsWith("/") ? splitPath : splitPath + "/"
+    path = outputPath? (outputPath.endsWith("/") ? outputPath : outputPath + "/") : splitPath;
     var outputFilePath = path + "merged_schema.graphql";
-    if (fs.existsSync(outputFilePath) && overWrite != "false") fs.unlinkSync(path + "merged_schema.graphql");
-
+    if (fs.existsSync(outputFilePath) && overWrite) fs.unlinkSync(path + "merged_schema.graphql");
+    var returnAST=null;
     //Traverse all possible types and write non-extendible definitions
-    var len = types.length;
-    for (var i = 0; i < len; i++) {
-        var typePath = splitPath + types[i] + "/";
-        if (!fs.existsSync(typePath)) continue;
-        if (!appendDefinitions(typePath, outputFilePath, avoidExtendibles)) return;
-    }
+    types.forEach(currentType => {
+        var typePath = splitPath + currentType + "/";
+        if (fs.existsSync( typePath)){
+            var files = fs.readdirSync(typePath);
+            //Iterate through folder files
+            var typeAST = null;
+            files.forEach(function (file) {
+                if (file.endsWith(".graphql")) {
+                    try {
+                        var name = file.split("_")[1].split(".")[0];
+                        var contentFile = fs.readFileSync(typePath + file).toString();
+                        var astFile=null;
+                        try {
+                            astFile = graphqllang.parse(contentFile); 
+                        } catch (error) {
+                            var regExp = new RegExp(name + "\s*?{(.*?)}", "is");
+                            var matches = contentFile.match(regExp);
+                            if (matches.length>1 && matches[1].replace(/(\r\n\t|\n|\s|\r\t)/gm,"")===""){
+                                regExp = new RegExp("(" + name + "\s*?{).*?(})", "is");
+                                contentFile=contentFile.replace(regExp, '$1xtgVirtual:Boolean$2');
+                            }
+                            try{
+                                astFile = graphqllang.parse(contentFile);
+                            }catch(error){
+                                console.log("Fail parsing file: " + file + " Error: " + error.message);
+                            }
 
-    //Write extendible definitions (if there is any)
-    if (avoidExtendibles === "true") {
-        extendibles.forEach(function (item) {
-            if (extendedTypes[item]) {
-                //Merge file
-                fs.appendFileSync(outputFilePath, extendedTypes[item].join("\n") + "\n");
-            }
-        });
-    }
-
-    console.log("Files successfuly merged at " + outputFilePath);
-
-    return extendedTypes;
-}
-
-//Deprecated
-function appendDefinitions(inPath, outFilePath, avoidExtendibles) {
-    //Get file names
-    var names = fs.readdirSync(inPath);
-
-    //Iterate through folder files
-    names.forEach(function (file) {
-        if (file.endsWith(".graphql")) {
-            var content = fs.readFileSync(inPath + file, 'utf-8');
-            var fileLines = content.toString().split('\n');
-
-            //Iterate until first significant line
-            for (var i = 0; i < fileLines.length; i++) {
-                var line = fileLines[i];
-                if (line.length <= 1) continue; //Remove empty lines
-                var split = line.split(' ');
-                if (!split[1]) continue;
-                var keyWord = split[0].trim();
-                var itemName = split[0] === "extend" ? split[2].trim() : split[1].trim();
-
-                //Fix possible not separated keys
-                if (itemName.substr(itemName.length - 1) == "{") itemName = itemName.substr(0, itemName.length - 1);
-
-                //If "extend" found, extend type
-                if (keyWord === "extend" && extendibles.indexOf(itemName) <= -1) {
-                    console.log("ERROR: Trying to extend an XTG non extendible type (" + itemName + ").");
-                    return false;
-
-                } else if (keyWord === "extend" || (extendibles.indexOf(itemName) > -1)) {
-                    var value = extendedTypes[itemName];
-                    value = fileLines;                                                                      //Add as complete definition
-                    if (avoidExtendibles != "true") value[i] = value[i].replace("extend", "");              //Remove "extend" keyword
-                    extendedTypes[itemName] = value;
-                    break;
-
-                } else if (keyWord === "#") {
-                    continue;
-
-                } else {
-                    //Merge file
-                    fs.appendFileSync(outFilePath, content + "\n");
-                    break;
+                        }
+                        if (astFile!==null){
+                            astFile.definitions.forEach(definition => {
+                                convertLegacyDescriptions(definition);
+                            });
+                            typeAST=typeAST!==null?graphql.concatAST([typeAST, astFile]): astFile;
+                        }
+                    } catch (error) {
+                        console.log("The file '" + file + "' doesnt have a correct name (type_Name.graphql)");    
+                    }
                 }
+            });
+            if (typeAST){
+                returnAST= returnAST!==null?graphql.concatAST([returnAST,typeAST]): typeAST;
             }
         }
+
     });
-    return true;
+    return returnAST;
 }
