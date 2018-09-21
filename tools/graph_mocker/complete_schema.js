db80de46bc3e5f28e96413bf159baa2f3e4314c0
Amian <damian@xmltravelgate.com>
2018-09-21 13:06:04 +0200

    Fix message

--- tools/graph_mocker/complete_schema.js
+++ tools/graph_mocker/complete_schema.js
@@ -1,7 +1,7 @@
 const { main } = require('./mocker')
 
 if (!process.argv[2]) { 
-    console.log("ERROR: No path or api was provided.");
+    console.log("ERROR: No path was provided.");
     return; 
 }
 main(process.argv[2], null);
