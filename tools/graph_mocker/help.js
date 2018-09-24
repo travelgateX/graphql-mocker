746d9b1bedccff66740a8fcaf3b1705956b5bf19
Amian <damian@xmltravelgate.com>
2018-09-24 15:19:58 +0200

    Fix helps files

--- tools/graph_mocker/help.js
+++ tools/graph_mocker/help.js
@@ -1,7 +1,22 @@
 module.exports.printMockerHelp = function() {
-    console.log("-> mock <directory> [<apiName> <workingAPI>]\n");
+    console.log("-> mock <directory> <apiPath>\n");
     console.log("       Mocks a schema and/or extends it with another one.\n");
     console.log("           <directory>: Path to the directory containing the schema/s.");
     console.log("           <apiPath>: Path of API (example: admin/iam, admin/core ...)");
+    console.log("\n\n")
+}
+
+module.exports.printExtractApiHelp = function() {
+    console.log("-> extract_api <directory> <apiPath>\n");
+    console.log("       Extract complete schema for API with all depends.\n");
+    console.log("           <directory>: Path to the directory containing the schema/s.");
+    console.log("           <apiPath>: Path of API (example: admin/iam, admin/core ...)");
+    console.log("\n\n")
+}
+
+module.exports.printCompleteSchemaHelp = function() {
+    console.log("-> complete_schema <directory>\n");
+    console.log("       Extract complete schema.\n");
+    console.log("           <directory>: Path to the directory containing the schema/s.");
     console.log("\n\n")
 }
